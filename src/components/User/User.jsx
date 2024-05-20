d7843472d187192969e4720fc6d521fb43379512
PdaDmitry <pda.gazcomp@gmail.com>
2024-05-20 10:52:28 +0300

    Icons

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,11 +1,19 @@
+import { FaUser } from 'react-icons/fa6';
+import { FaPhoneAlt } from 'react-icons/fa';
 import css from './User.module.css';
 
 export default function User({ data: { id, name, number }, onDelete }) {
   return (
     <div className={css.contContact}>
       <div>
-        <p>{name}</p>
-        <p>{number}</p>
+        <p>
+          <FaUser className={css.iconUser} size={14} />
+          {name}
+        </p>
+        <p>
+          <FaPhoneAlt className={css.iconPhone} size={14} />
+          {number}
+        </p>
       </div>
 
       <button onClick={() => onDelete(id)}>Delete</button>
